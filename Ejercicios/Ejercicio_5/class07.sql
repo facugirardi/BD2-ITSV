51379dea05eb80273acb6a80a983e5924ed5610d
facugirardi <facugirardi22@gmail.com>
2024-05-13 11:22:22 -0300

    class07

---
+++ Ejercicios/Ejercicio_5/class07.sql
@@ -0,0 +1,18 @@
+USE sakila;
+
+SELECT title, rating FROM film WHERE LENGTH =  (SELECT MIN(`length`) FROM film);
+
+SELECT title FROM film f1 WHERE LENGTH < ALL (SELECT LENGTH FROM film f2 WHERE f1.film_id != f2.film_id);
+
+SELECT c.*, MIN(p.amount) AS lowest_amount,a.* FROM payment p
+INNER JOIN customer c ON c.customer_id=p.customer_id
+INNER JOIN ADDRESS a ON c.address_id=a.address_id 
+GROUP BY p.customer_id;
+
+SELECT p.amount AS lowest_amount,c.*,a.* FROM payment p
+INNER JOIN customer c ON c.customer_id=p.customer_id
+INNER JOIN ADDRESS a ON c.address_id=a.address_id 
+WHERE p.amount <= all(SELECT amount FROM payment p2 WHERE p.customer_id=p2.customer_id)
+GROUP BY p.customer_id, p.amount;
+
+SELECT c.*, (SELECT MAX(p.amount) FROM payment p WHERE c.customer_id=p.customer_id) AS highest, (SELECT MIN(amount) FROM payment p WHERE c.customer_id=p.customer_id) AS lowest FROM customer c;
